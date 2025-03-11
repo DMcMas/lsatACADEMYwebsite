ab0475ec7aa5c680dc7e9479d2cc7a74b8fe3481
Josiah Dalisay <70873833+D-Josiah@users.noreply.github.com>
2025-03-12 05:48:16 +0800

    build 2

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/dist/', // Ensures assets are correctly linked to /dist/
-  build: {
-    outDir: 'dist', // Output files inside dist/
-    assetsDir: 'assets', // Keep assets inside dist/assets/
-    rollupOptions: {
-      output: {
-        entryFileNames: 'assets/[name]-[hash].js', // Keeps JS in assets folder
-        chunkFileNames: 'assets/[name]-[hash].js',
-        assetFileNames: 'assets/[name]-[hash][extname]',
-      },
-    },
-  },
+  base: './', // Ensures assets are correctly linked
 });
